06fae24ff7ec17e7cc0516609978cda480bf5f24
wehr <mariowehr@gmx.at>
2015-05-26 13:03:21 +0200

    Added format_string() call to intro text processing so multilang filters will work.

--- view.php
+++ view.php
@@ -486,7 +486,7 @@ echo $OUTPUT->heading ( format_string ( $groupselect->name, true, array (
 
 if (trim ( strip_tags ( $groupselect->intro ) )) {
 	echo $OUTPUT->box_start ( 'mod_introbox', 'groupselectintro' );
-	echo format_module_intro ( 'page', $groupselect, $cm->id );
+    echo format_string ( format_module_intro ( 'page', $groupselect, $cm->id ) );
 	echo $OUTPUT->box_end ();
 }
 
